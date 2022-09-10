49c305e7125054a2b55d9c80d96d478936c0d18b
Mikhail Belov <55504815+MikkiWhiteDove@users.noreply.github.com>
2022-09-10 11:23:48 +0300

    addProjFile

---
+++ NetflixCloneSwift/NetflixCloneSwift/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  NetflixCloneSwift
+//
+//  Created by Mishana on 26.02.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ NetflixCloneSwift/NetflixCloneSwift/Controllers/UpcomingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpcomingViewController.swift
+//  NetflixCloneSwift
+//
+//  Created by Mishana on 26.02.2022.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
